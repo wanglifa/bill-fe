0794b8aa58612a6d3d38a3e7e0e69de90ec2db4a
wanglifa <wanglifa1995@gmail.com>
2022-05-03 20:44:07 +0800

    feat: 引入 vue router 4

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/bill-fe/dist/',
   plugins: [
     vue(),
     vueJsx({
