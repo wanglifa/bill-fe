cd1fb8126372ea4200cf28a6134da0e1f8f85337
wanglifa <wanglifa1995@gmail.com>
2022-05-04 15:43:27 +0800

    chore: 安装 sass

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,10 +1,15 @@
 import { defineComponent } from 'vue';
 import { RouterView } from 'vue-router';
+import s from './Welcome.module.scss';
 export const Welcome = defineComponent({
   setup: (props, context) => {
     return () => (
-      <div>
+      <div class={s.wrapper}>
+        <header>logo</header>
+        <main>
         <RouterView />
+        </main>
+        <footer>buttons</footer>
       </div>
     )
   }
