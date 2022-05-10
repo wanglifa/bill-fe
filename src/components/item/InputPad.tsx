85554ac315b295fb629fbbc4cabf984d617f5e94
wanglifa <wanglifa1995@gmail.com>
2022-05-10 17:54:13 +0800

    feat: 使用 vant 日期组件

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -1,6 +1,8 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
 import { Icon } from '../../shared/Icon';
+import { time } from '../../shared/time';
 import s from './InputPad.module.scss';
+import { DatetimePicker, Popup } from 'vant';
 export const InputPad = defineComponent({
   props: {
     name: {
@@ -8,6 +10,8 @@ export const InputPad = defineComponent({
     }
   },
   setup: (props, context) => {
+    const now = new Date()
+    const refDate = ref<Date>(now)
     const buttons = [
       { text: '1', onClick: () => { } },
       { text: '2', onClick: () => { } },
@@ -26,11 +30,18 @@ export const InputPad = defineComponent({
       { text: '删', onClick: () => { } },
       { text: '提交', onClick: () => { } },
     ]
+    const refShowPop = ref(false)
     return () => <>
       <div class={s.dateAndAmount}>
         <span class={s.date}>
-          <Icon name="date" class={s.icon}/>
-          <span>2022-01-01</span>
+          <Icon name="date" class={s.icon} />
+          <span>
+            <span onClick={() => refShowPop.value = true}>{time(refDate.value).format()}</span>
+            <Popup position='bottom' v-model:show={refShowPop.value}>
+              <DatetimePicker v-model={refDate.value} type="date" title="选择年月日"
+                onConfirm={() => refShowPop.value = false} />
+            </Popup>
+          </span>
         </span>
         <span class={s.amount}>199.12</span>
       </div>
