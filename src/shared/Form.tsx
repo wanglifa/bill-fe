6ceee14fd594769232743b3fabbdba1d04e783a2
wanglifa <wanglifa1995@gmail.com>
2022-05-28 15:35:15 +0800

    feat: 自定义时间弹窗

--- src/shared/Form.tsx
+++ src/shared/Form.tsx
@@ -1,10 +1,12 @@
-import { computed, defineComponent, PropType } from "vue";
-import { EmojiSelect } from "./EmojiSelect";
+import { DatetimePicker, Popup } from 'vant';
+import { computed, defineComponent, PropType, ref, VNode } from 'vue';
+import { EmojiSelect } from './EmojiSelect';
 import s from './Form.module.scss';
+import { Time } from './time';
 export const Form = defineComponent({
   props: {
     onSubmit: {
-      type: Function as PropType<(e: Event) => void>
+      type: Function as PropType<(e: Event) => void>,
     }
   },
   setup: (props, context) => {
@@ -15,61 +17,69 @@ export const Form = defineComponent({
     )
   }
 })
+
 export const FormItem = defineComponent({
   props: {
     label: {
-      type: String as PropType<string>,
+      type: String
     },
     modelValue: {
-      type: [String, Number] as PropType<string | number>,
+      type: [String, Number]
     },
     type: {
       type: String as PropType<'text' | 'emojiSelect' | 'date'>,
     },
     error: {
-      type: String as PropType<string>,
+      type: String
     }
   },
   setup: (props, context) => {
+    const refDateVisible = ref(false)
     const content = computed(() => {
       switch (props.type) {
         case 'text':
-          return <input 
+          return <input
             value={props.modelValue}
-            onInput={(e: any) => {context.emit('update:modelValue', e.target.value)}}
-          />
+            onInput={(e: any) => context.emit('update:modelValue', e.target.value)}
+            class={[s.formItem, s.input]} />
         case 'emojiSelect':
-          return <EmojiSelect 
+          return <EmojiSelect
             modelValue={props.modelValue?.toString()}
-            onUpdateModelValue={(value: string) => {context.emit('update:modelValue', value)}}
-            class={[s.formItem, s.emojiList, s.error]}
-          />
+            onUpdateModelValue={value => context.emit('update:modelValue', value)}
+            class={[s.formItem, s.emojiList, s.error]} />
         case 'date':
-          return <input />
+          return <>
+            <input readonly={true} value={props.modelValue}
+              onClick={() => { refDateVisible.value = true }}
+              class={[s.formItem, s.input]} />
+            <Popup position='bottom' v-model:show={refDateVisible.value}>
+              <DatetimePicker value={props.modelValue} type="date" title="选择年月日"
+                onConfirm={(date: Date) => {
+                  context.emit('update:modelValue', new Time(date).format())
+                  refDateVisible.value = false
+                }}
+                onCancel={() => refDateVisible.value = false} />
+            </Popup></>
         case undefined:
           return context.slots.default?.()
       }
     })
-    return () => (
-      <div class={s.formRow}>
+    return () => {
+      return <div class={s.formRow}>
         <label class={s.formLabel}>
-          {
-            props.label && (
-              <span class={s.formItem_name}>{props.label}</span>
-            )
+          {props.label &&
+            <span class={s.formItem_name}>{props.label}</span>
           }
           <div class={s.formItem_value}>
             {content.value}
           </div>
-          {
-            props.error && (
-              <div class={s.formItem_errorHint}>
-                <span>{props.error}</span>
-              </div>
-            )
+          {props.error &&
+            <div class={s.formItem_errorHint}>
+              <span>{props.error}</span>
+            </div>
           }
         </label>
       </div>
-    )
+    }
   }
 })
