608b7b5c18de3d733b3e78a5462b073466859892
wanglifa <wanglifa1995@gmail.com>
2022-05-28 14:05:31 +0800

    feat: 实现 ItemSummary

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import { RouterView } from "vue-router";
 import "./App.scss"
 
 export const App = defineComponent({
-  setup() {
-    return () => (
-      <div class="page">
-        <RouterView />
-      </div>
-    )
-  }
+    setup() {
+      return () => (
+        <div class="page">
+          <RouterView />
+        </div>
+      )
+    }
 })
