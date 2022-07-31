cc7dfdd7f0bfd429a49146df3bc6706bd5481ff1
wanglifa <wanglifa1995@gmail.com>
2022-07-31 17:14:28 +0800

    feat: 使用 mock

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -36,9 +36,7 @@ export const SignInPage = defineComponent({
         { key: 'code', type: 'required', message: '必填' },
       ]))
       if(!hasError(errors)){
-        const response = await http.post<{jwt: string}>('/session', formData, {
-          params: { _mock: 'session' }
-        })
+        const response = await http.post<{jwt: string}>('/session', formData)
         .catch(onError)
         localStorage.setItem('jwt', response.data.jwt)
         const returnTo = route.query.return_to?.toString()
