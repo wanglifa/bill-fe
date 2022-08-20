9ecf5f2f149f73d047aedfb21ae9a06004806780
wanglifa <wanglifa1995@gmail.com>
2022-08-20 20:55:25 +0800

    feat: 加载第一页 tag

--- src/env.d.ts
+++ src/env.d.ts
@@ -8,3 +8,19 @@ declare module '*.vue' {
 }
 
 type JSONValue = null | boolean | string | number | JSONValue[] | Record<string, JSONValue>
+type Tag = {
+  id: number,
+  user_id: number,
+  name: string,
+  sign: string,
+  kind: expenses | income
+}
+
+type Resources<T = any> = {
+  resources: T[]
+  pager: {
+    page: number,
+    per_page: number,
+    count: number
+  }
+} 
