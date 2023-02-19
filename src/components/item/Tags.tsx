92a368b669f55a573f35f5eb6c27b24eba5529eb
wanglifa <wanglifa1995@gmail.com>
2023-02-19 16:48:11 +0800

    feat: 长按 tag 跳转到编辑页面

--- src/components/item/Tags.tsx
+++ src/components/item/Tags.tsx
@@ -1,5 +1,5 @@
 import { defineComponent, onUpdated, PropType, ref } from 'vue';
-import { RouterLink } from 'vue-router';
+import { RouterLink, useRouter } from 'vue-router';
 import { Button } from '../../shared/Button';
 import { http } from '../../shared/Http';
 import { Icon } from '../../shared/Icon';
@@ -15,8 +15,6 @@ export const Tags = defineComponent({
   },
   emits: ['update:selected'],
   setup: (props, context) => {
-    const timer = ref<number>()
-    const currentTag = ref<HTMLDivElement>()
     const { tags, hasMore, page, fetchTags } = useTags((page) => {
       return http.get<Resources<Tag>>('/tags', {
         kind: props.kind,
@@ -27,13 +25,17 @@ export const Tags = defineComponent({
     const onSelect = (tag: Tag) => {
       context.emit('update:selected', tag.id);
     };
-    const onLongPress = () => {
-      console.log('长按')
+    const timer = ref<number>()
+    const currentTag = ref<HTMLDivElement>()
+
+    const router = useRouter()
+    const onLongPress = (tagId: Tag['id'])=>{
+      router.push(`/tags/${tagId}/edit?kind=${props.kind}&return_to=${router.currentRoute.value.fullPath}`)
     }
-    const onTouchStart = (e: TouchEvent) => {
+    const onTouchStart = (e: TouchEvent, tag: Tag) => {
       currentTag.value = e.currentTarget as HTMLDivElement
       timer.value = setTimeout(()=>{
-        onLongPress()
+        onLongPress(tag.id)
       }, 500)
     }
     const onTouchEnd = (e: TouchEvent) => {
@@ -59,7 +61,7 @@ export const Tags = defineComponent({
             <div
               class={[s.tag, props.selected === tag.id ? s.selected : '']}
               onClick={() => onSelect(tag)}
-              onTouchstart={onTouchStart}
+              onTouchstart={(e)=>onTouchStart(e, tag)}
               onTouchend={onTouchEnd}
             >
               <div class={s.sign}>{tag.sign}</div>
